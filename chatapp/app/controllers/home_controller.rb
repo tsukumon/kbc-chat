7c810bc4334784fb1495d151268841118a1f7f1d
Hiroki Ohtake <hametu1992@gmail.com>
2022-04-25 14:05:16 +0900

    edit1

--- chatapp/app/controllers/home_controller.rb
+++ chatapp/app/controllers/home_controller.rb
@@ -1,2 +1,17 @@
 class HomeController < ApplicationController
+    def home
+        
+    end
+
+    def rooms
+        
+    end
+
+    def settings
+
+    end
+
+    def contact
+
+    end
 end
--- chatapp/config/routes.rb
+++ chatapp/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get "/" => "home#index"
+  get "/rooms" => "home#rooms"
+  get "/settings" => "home#settings"
+  get "/contact" => "home#contact"
   # Defines the root path route ("/")
   # root "articles#index"
 end
