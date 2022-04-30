f84fd1416cfcd59f4a56798e5280f3072ea68563
tsukumon <syou.1225st@gmail.com>
2022-04-30 19:17:36 +0900

    write test code

--- chatapp/spec/models/room_spec.rb
+++ chatapp/spec/models/room_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  before do 
+    #モデルのみ作成
+    @room = build(:room)
+  end
+
+  describe 'バリデーション' do
+    it 'nameとdescribeどちらも値が設定されていればOK' do
+      expect(@room.valid?).to eq(true)
+    end
+
+    it 'nameが空だとNG' do
+      @room.name = ''
+      expect(@room.valid?).to eq(false)
+    end
+
+    it 'describeが空だとNG' do
+      @room.describe = ''
+      expect(@room.valid?).to eq(false)
+    end
+  end
 end
