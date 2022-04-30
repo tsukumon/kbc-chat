f4a24145a1515eed35cc5fd3c251b23990fc7617
tsukumon <syou.1225st@gmail.com>
2022-04-30 19:12:28 +0900

    create rooms model

--- chatapp/db/schema.rb
+++ chatapp/db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_04_30_101155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rooms", force: :cascade do |t|
+    t.text "name"
+    t.text "describe"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
