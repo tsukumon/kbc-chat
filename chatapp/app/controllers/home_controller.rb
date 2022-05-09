438de53d31d4fc5d241af5f75d0fe6f31ed76d3f
Hiroki Ohtake <hametu1992@gmail.com>
2022-05-09 10:15:01 +0900

    edit home controller

--- chatapp/app/controllers/home_controller.rb
+++ chatapp/app/controllers/home_controller.rb
@@ -2,4 +2,12 @@ class HomeController < ApplicationController
     def index
         @rooms = Room.all
     end
+
+    def get
+
+    end
+
+    def create
+        @message = Message.new(sentence: params[:message], room_id: params[:id])
+    end
 end
