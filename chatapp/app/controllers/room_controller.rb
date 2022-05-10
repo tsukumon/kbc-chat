1ed4b07ae8188312fe9f16ef420de8b214c0f1a6
tsukumon <syou.1225st@gmail.com>
2022-05-10 13:56:23 +0900

    write: room page

--- chatapp/app/controllers/room_controller.rb
+++ chatapp/app/controllers/room_controller.rb
@@ -1,13 +1,17 @@
 class RoomController < ApplicationController
-
   #room新規作成ページ（GET）
-  def new_room
+  def new
     #code
   end
 
   #room新規作成処理（POST）
-  def create_room
+  def create
     #code
+  end
+ 
+  #ルームページ（個別)
+  def page
+    @messages = Message.find_by(room_id: params[:id])
   end
 
 end
--- chatapp/config/routes.rb
+++ chatapp/config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
+  #top page
   get "/" => "home#index"
-  # Defines the root path route ("/")
-  # root "articles#index"
 
+  #rooms
   get "/room" => "room#index"
   get "/room/new" => "room#new"
   post "/room/create" => "room#create"
+
+  #room page
+  get "/room/:id" => "room#page"
+  
+
+
 end
