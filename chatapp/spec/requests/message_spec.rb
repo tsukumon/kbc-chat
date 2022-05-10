f22cf62c60038537503b4a70d45f9ef8da44a015
Hiroki Ohtake <hametu1992@gmail.com>
2022-05-10 14:03:01 +0900

    edit views

--- chatapp/spec/requests/message_spec.rb
+++ chatapp/spec/requests/message_spec.rb
@@ -2,6 +2,27 @@ require 'rails_helper'
 
 RSpec.describe "Messages", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    before do
+      @message = post(:message)
+    end
+
+    it "response HTTP 200" do
+      get message_path
+      expect(response).to be_successfull
+    end
+
+    #pending "add some examples (or delete) #{__FILE__}"
   end
+
+  describe "message post action" do
+    it "redirect to " do
+      post room_post_path, :params => {:sentence => "test-message"}
+      expect(response).to redirect_to room_post_path
+    end
+  end
+
+  describe "m" do
+
+  end
+
 end
