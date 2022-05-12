73fd1002390627224faebef0a581b6cad0f0ee94
Hiroki Ohtake <hametu1992@gmail.com>
2022-05-12 14:25:50 +0900

    add japanese locale settings

--- chatapp/config/application.rb
+++ chatapp/config/application.rb
@@ -28,5 +28,7 @@ module Myapp
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+    #config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
