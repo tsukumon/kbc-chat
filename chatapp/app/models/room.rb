c0df95114194ebf3b63e429cd403f9f7967a3a99
Hiroki Ohtake <hametu1992@gmail.com>
2022-05-13 14:50:16 +0900

    install carrierwave&minimagick, roomuploader

--- chatapp/app/models/room.rb
+++ chatapp/app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
   validates :name, {presence: true, length: {maximum: 30}}
   validates :describe, {presence: true, length: {maximum: 400}}
+  validates :image, presence: true
+  mount_uploader :image, RoomUploader
 end
