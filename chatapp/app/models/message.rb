89df21585845a873ab50896a99b27a10c921a02e
Hiroki Ohtake <hametu1992@gmail.com>
2022-05-13 13:37:17 +0900

    edit validation text length

--- chatapp/app/models/message.rb
+++ chatapp/app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ApplicationRecord
-    validates :sentence, presence: true
+    validates :sentence, {presence: true, length: {maximum: 2000}}
     validates :room_id, presence: true
 end
