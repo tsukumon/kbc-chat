d04e281b017f8cd742224571d7a462f61e3341f1
Hiroki Ohtake <hametu1992@gmail.com>
2022-05-13 15:41:43 +0900

    edit validates for seeds error

--- chatapp/app/models/room.rb
+++ chatapp/app/models/room.rb
@@ -1,6 +1,5 @@
 class Room < ApplicationRecord
   validates :name, {presence: true, length: {maximum: 30}}
   validates :describe, {presence: true, length: {maximum: 400}}
-  validates :image, presence: true
   mount_uploader :image, RoomUploader
 end
--- chatapp/app/uploaders/room_uploader.rb
+++ chatapp/app/uploaders/room_uploader.rb
@@ -13,6 +13,15 @@ class RoomUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def filename
+    "#{secure_token}.png" if original_filename.present?
+  end
+
+  protected
+  def default_url
+    "/default.jpg"
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
@@ -35,9 +44,9 @@ class RoomUploader < CarrierWave::Uploader::Base
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_allowlist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
