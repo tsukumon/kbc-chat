b8c910af89df80225c1f0c7cfffa886426e40ffc
Hiroki Ohtake <hametu1992@gmail.com>
2022-05-13 21:12:29 +0900

    add uploader sizerange

--- chatapp/app/uploaders/room_uploader.rb
+++ chatapp/app/uploaders/room_uploader.rb
@@ -22,6 +22,12 @@ class RoomUploader < CarrierWave::Uploader::Base
     "/default.jpg"
   end
 
+  def size_range
+    1..10.megabytes
+  end
+
+
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
