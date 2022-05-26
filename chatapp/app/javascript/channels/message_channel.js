7364276dcaac1c2e3131095e7534292bc01c0c10
tsukumon <syou.1225st@gmail.com>
2022-05-26 13:58:40 +0900

    fixed

--- chatapp/app/javascript/channels/message_channel.js
+++ chatapp/app/javascript/channels/message_channel.js
@@ -9,7 +9,7 @@ consumer.subscriptions.create("MessageChannel", {
     // Called when the subscription has been terminated by the server
   },
 
-  received(data,time) {
+  received(data) {
     //print(data)
     // Called when there's incoming data on the websocket for this channel
     //message
