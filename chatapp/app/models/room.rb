821a0d2aa75b5e8f846412591442212603b1bbaa
tsukumon <syou.1225st@gmail.com>
2022-06-06 19:41:46 +0900

    describe fixed

--- chatapp/app/models/room.rb
+++ chatapp/app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   has_many :message, dependent: :destroy
   validates :name, {presence: true, length: {maximum: 30}}
-  validates :describe, {presence: true, length: {maximum: 400}}
+  validates :describe, {length: {maximum: 400}}
   mount_uploader :image, RoomUploader
 end
