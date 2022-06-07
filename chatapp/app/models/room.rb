773c73ba7d9f302ce60fb46118fcf3306b03ef63
Hiroki Ohtake <hametu1992@gmail.com>
2022-06-07 10:58:26 +0900

    add valid

--- chatapp/app/models/room.rb
+++ chatapp/app/models/room.rb
@@ -2,6 +2,7 @@ class Room < ApplicationRecord
   has_many :message, dependent: :destroy
   validates :name, {presence: true, length: {maximum: 30}}
   validates :describe, {length: {maximum: 400}}
-  validates :category, presence: true
+  VALID_CATEGORY = /\AQ&A\z|\A募集\z|\A雑談\z|\Aプログラミング\z|\AOS\z|\Aクラウド\z/
+  validates :category, {presence: true, format: {with: VALID_CATEGORY}}
   mount_uploader :image, RoomUploader
 end
