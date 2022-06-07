bbd5d84f737546236b3b0d409e97c1864af100a5
tsukumon <syou.1225st@gmail.com>
2022-06-07 15:02:32 +0900

    fixed

--- chatapp/app/models/room.rb
+++ chatapp/app/models/room.rb
@@ -5,7 +5,7 @@ class Room < ApplicationRecord
   #VALID_CATEGORY = /\AQ&A\z|\A募集\z|\A雑談\z|\Aプログラミング\z|\AOS\z|\Aクラウド\z/
   validates :name, {presence: true, length: {maximum: 30}}
   validates :describe, {length: {maximum: 400}}
-  validates :category, {presence: true}
+  validates :category, {presence: true, length: {maximum: 15}}
 
   scope :by_category_like, lambda { |category|
   where('category LIKE :value', { value: "#{sanitize_sql_like(category)}%"})
