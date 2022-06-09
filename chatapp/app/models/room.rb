80ec7ce473a04ecd9dd86459d5564f8c7d0eca7c
Hiroki Ohtake <hametu1992@gmail.com>
2022-06-09 13:55:03 +0900

    部分一致

--- chatapp/app/models/room.rb
+++ chatapp/app/models/room.rb
@@ -12,6 +12,6 @@ class Room < ApplicationRecord
   }
 
   scope :by_name_like, lambda { |name|
-  where('name LIKE :value', { value: "#{sanitize_sql_like(name)}%"})
+  where('name LIKE :value', { value: "%#{sanitize_sql_like(name)}%"})
   }
 end
