cae1780c20a2fc59bd29462aa8907b104f4f1fc0
tsukumon <syou.1225st@gmail.com>
2022-06-09 13:30:55 +0900

    fixed

--- chatapp/app/javascript/application.js
+++ chatapp/app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require jquery-ui/widgets/autocomplete
+//= require jquery.turbolinks
 import "@hotwired/turbo-rails"
 import "controllers"
 import "channels"
