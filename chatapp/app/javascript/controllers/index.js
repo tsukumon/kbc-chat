ba77478f4ed0a2008e1f08ee861f437ed287f1d5
tsukumon <syou.1225st@gmail.com>
2022-06-09 23:40:02 +0900

    fixed

--- chatapp/app/javascript/controllers/index.js
+++ chatapp/app/javascript/controllers/index.js
@@ -11,14 +11,8 @@ eagerLoadControllersFrom("controllers", application)
 // lazyLoadControllersFrom("controllers", application)
 function flexTextarea(el) {
     const dummy = el.querySelector('.dummy-textarea')
-    const margin = document.getElementById('dummy-margin')
-    const height = document.getElementById('message-sentence').clientHeight
     el.querySelector('.message-sentense-class').addEventListener('input', e => {
       dummy.textContent = e.target.value + '\u200b'
-      height = document.getElementById('message-sentence').clientHeight
-      margin.style.marginBottom = height
-      console.log(height)
-      console.log(margin.style.marginBottom)
     })
   }
 document.querySelectorAll('.message-textarea').forEach(flexTextarea)
