53bdd4e70f68c680891525faa2e70b3c1a1beb49
tsukumon <syou.1225st@gmail.com>
2022-06-09 22:35:59 +0900

    fixed

--- chatapp/app/javascript/controllers/index.js
+++ chatapp/app/javascript/controllers/index.js
@@ -9,3 +9,10 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+function flexTextarea(el) {
+    const dummy = el.querySelector('.dummy-textarea')
+    el.querySelector('.message-sentense-class').addEventListener('input', e => {
+      dummy.textContent = e.target.value + '\u200b'
+    })
+  }
+document.querySelectorAll('.message-textarea').forEach(flexTextarea)
