b63e92085b4e868b0446fc7dc6c00bdb7c7afac2
w-record <keita_0129_n@yahoo.co.jp>
2022-06-13 12:07:31 +0900

    ittan

--- chatapp/app/javascript/controllers/application.js
+++ chatapp/app/javascript/controllers/application.js
@@ -7,3 +7,24 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+
+var modal = document.getElementById('demo-modal');
+var btn = document.getElementById('open-modal');
+var close = modal.getElementsByClassName('close')[0];
+
+// When the user clicks the button, open the modal.
+btn.onclick = function() {
+  modal.style.display = 'block';
+};
+// When the user clicks on 'X', close the modal
+close.onclick = function() {
+  modal.style.display = 'none';
+};
+// When the user clicks outside the modal -- close it.
+window.onclick = function(event) {
+  if (event.target == modal) {
+    // Which means he clicked somewhere in the modal (background area), but not target = modal-content
+    modal.style.display = 'none';
+  }
+};
