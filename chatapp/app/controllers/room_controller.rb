ff89463a3862c622fc0219f581102d6bfa66852c
yuiki-oshiro <yuikio1001@gmail.com>
2022-07-14 05:38:11 +0000

    めいめい

--- chatapp/app/controllers/room_controller.rb
+++ chatapp/app/controllers/room_controller.rb
@@ -43,8 +43,8 @@ class RoomController < ApplicationController
     @messages = Message.where(room_id: params[:id]).order(created_at: :DESC).page(params[:page]).per(30)
     @message = Message.new
 
-    @members_id = UserRoom.where(room_id: params[:id]).pluck(:user_id)
-    @members = User.where(id: @members_id)
+    members_id = UserRoom.where(room_id: params[:id]).pluck(:user_id)
+    @members = User.where(id: members_id)
     @members_hash = @members.map{ |user| [user.id, user.attributes]}.to_h
   end
 
