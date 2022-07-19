577cec49b9c0f08c5135a3014183682dd678e43c
Hiroki Ohtake <hametu1992@gmail.com>
2022-07-19 13:59:40 +0900

    fixed

--- chatapp/app/javascript/controllers/index.js
+++ chatapp/app/javascript/controllers/index.js
@@ -2,20 +2,23 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
-function flexTextarea(el) {
-  const dummy = el.querySelector('.dummy-textarea')
-  const dummy2 = $('.dummy-margin')
-  el.querySelector('.message-sentense-class').addEventListener('input', e => {
-    dummy.textContent = e.target.value + '\u200b'
-    var dummy_height = $('.dummy-textarea').height() - 40
-    dummy2.css("height", dummy_height + "px")
-    //window.scrollTo(0, document.body.scrollHeight);
-  })
-}
-document.querySelectorAll('.room-content').forEach(flexTextarea)
+document.addEventListener("turbo:load", () => {
+  function flexTextarea(el) {
+    const dummy = el.querySelector('.dummy-textarea')
+    const dummy2 = $('.dummy-margin')
+    el.querySelector('.message-sentense-class').addEventListener('input', e => {
+      dummy.textContent = e.target.value + '\u200b'
+      var dummy_height = $('.dummy-textarea').height() - 40
+      dummy2.css("height", dummy_height + "px")
+      //window.scrollTo(0, document.body.scrollHeight);
+    })
+  }
+  document.querySelectorAll('.room-content').forEach(flexTextarea)
+});
 
 document.addEventListener("turbo:load", () => {
   function popupImage() {
+    Turbo.clearCache();
     var popup = document.getElementById('js-modal-detail');
     if(!popup) return;
 
