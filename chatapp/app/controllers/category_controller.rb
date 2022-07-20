97238b4abf7a3e58819a4e82e58eab6f7cc339fd
Hiroki Ohtake <hametu1992@gmail.com>
2022-07-20 12:04:05 +0900

    change

--- chatapp/app/controllers/category_controller.rb
+++ chatapp/app/controllers/category_controller.rb
@@ -2,7 +2,9 @@ class CategoryController < ApplicationController
   def index
     category = CGI.unescape(params[:category])
     @joined_rooms = UserRoom.where(user_id: @current_user.id).pluck(:room_id)
-    @rooms = Room.where(id: @joined_rooms, category: category)
+    @rooms = Room.where(category: category)
+    @rooms_joined = Room.where(id: @joined_rooms, category: category)
     @rooms_hash = @rooms.map{ |room| [room.id, room.attributes]}.to_h
+    @rooms_joined_hash = @rooms_joined.map{ |room| [room.id, room.attributes]}.to_h
   end
 end
