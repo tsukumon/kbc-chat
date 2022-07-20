ac705dee5717cca969090b52ee72856f9e303bc8
yuiki-oshiro <yuikio1001@gmail.com>
2022-07-20 01:00:00 +0000

    sort-categories

--- chatapp/app/controllers/category_controller.rb
+++ chatapp/app/controllers/category_controller.rb
@@ -1,7 +1,7 @@
 class CategoryController < ApplicationController
   def index
-    @category = Room.all
-    @category = @category.where(category: params[:id])
-    p @category
+    @joined_rooms = UserRoom.where(user_id: @current_user.id).pluck(:room_id)
+    @rooms = Room.where(id: @joined_rooms, category: params[:id])
+    @rooms_hash = @rooms.map{ |room| [room.id, room.attributes]}.to_h
   end
 end
