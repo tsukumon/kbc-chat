ffc042cab4b576ab854d10846aeb4c639d0eebcd
Hiroki Ohtake <hametu1992@gmail.com>
2022-08-31 09:47:02 +0900

    change display category name

--- chatapp/app/controllers/category_controller.rb
+++ chatapp/app/controllers/category_controller.rb
@@ -1,6 +1,7 @@
 class CategoryController < ApplicationController
   def index
     category = CGI.unescape(params[:category])
+    @category_name = params[:category_name]
     @joined = UserRoom.where(user_id: @current_user.id).pluck(:room_id)
     @rooms = Room.where.not(id: @joined).where(category: category)
     @rooms_joined = Room.where(id: @joined, category: category)
