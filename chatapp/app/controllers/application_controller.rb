8f6afd422267f851f9e9064ee257a8ecb3f68e44
Hiroki Ohtake <hametu1992@gmail.com>
2022-09-02 09:43:12 +0900

    fix set_current_user

--- chatapp/app/controllers/application_controller.rb
+++ chatapp/app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :set_current_user, :new_room
 
   def set_current_user
-    @current_user = User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def authenticate_user
