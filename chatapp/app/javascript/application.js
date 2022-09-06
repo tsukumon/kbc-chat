5ae3ae1055a5b03dff2badb051c502a602f79ec6
Hiroki Ohtake <hametu1992@gmail.com>
2022-09-06 10:43:48 +0900

    new infinite scroll

--- chatapp/app/javascript/application.js
+++ chatapp/app/javascript/application.js
@@ -1,6 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require jquery-ui/widgets/autocomplete
 //= require jquery.turbolinks
+//= require jquery.jscroll.min.js
 import "@hotwired/turbo-rails"
 import "controllers"
 import "channels"
